77bb10c2a917061ee708eb56110875d9da993bde
Ankit <ankit.ac.chheda@gmail.com>
2013-04-30 01:42:24 -0400

    Commiting before pulling from Khushal.

--- src/com/example/myvirtualcardwallet/Edit_card.java
+++ src/com/example/myvirtualcardwallet/Edit_card.java
@@ -3,6 +3,8 @@ package com.example.myvirtualcardwallet;
 import java.io.ByteArrayOutputStream;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -10,6 +12,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,35 +32,35 @@ import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 public class Edit_card extends Activity {
-	
+
 	public ParseObject localCard;
-	private static final int SELECT_PICTURE = 1;
 	private Bitmap bitmap;
 	private ImageView imageView;
 	private String newImagePath;
 	ParseFile photoFile;
-
+	private final int CAMERA_PICTURE = 1;
+	private final int GALLERY_PICTURE = 2;
+	private Intent pictureActionIntent = null;
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_card);
 		Parse.initialize(this, "HIWiddAFOEElxePoa7qHp72CHvwtNuqOXf1bXkjf", "aJpMrnM1WfUOpLGGSBUSl4pLPh4vVSdQrgL4VBi3"); 
 		ParseAnalytics.trackAppOpened(getIntent());
-		
+
 		Bundle b=this.getIntent().getExtras();
 		String username=b.getString("Username").toString();
-		Toast.makeText(getApplicationContext(),username, 
-				          Toast.LENGTH_SHORT).show();
+		imageView=(ImageView)findViewById(R.id.edit_card_image);
 
 
-		
+
 		final ParseQuery query = new ParseQuery("Card");
 		query.whereEqualTo("Username", username);
 		query.getFirstInBackground(new GetCallback(){
 
 			public void done(ParseObject foundCard, ParseException arg1) {
 				localCard=foundCard;
-			
+
 				EditText name= (EditText)findViewById(R.id.editcard_name);
 				name.setText(foundCard.getString("Name").toString());
 
@@ -75,19 +78,19 @@ public class Edit_card extends Activity {
 
 				EditText telephone= (EditText)findViewById(R.id.editcard_telephone);
 				telephone.setText(foundCard.getString("Telephone").toString());
-				
-				
-				 photoFile= (ParseFile)foundCard.get("Photo");
-				 photoFile.getDataInBackground(new GetDataCallback() {
-				  public void done(byte[] data, ParseException e) {
-				    if (e == null) {
-				      bitmap=BitmapFactory.decodeByteArray(data, 0, data.length);
-				      ImageView image=(ImageView) findViewById(R.id.edit_photo_image);
-				      image.setImageBitmap(bitmap);
-				    } else {
-				      // something went wrong
-				    }
-				  }
+
+
+				photoFile= (ParseFile)foundCard.get("Photo");
+				photoFile.getDataInBackground(new GetDataCallback() {
+					public void done(byte[] data, ParseException e) {
+						if (e == null) {
+							bitmap=BitmapFactory.decodeByteArray(data, 0, data.length);
+							ImageView image=(ImageView) findViewById(R.id.edit_photo_image);
+							image.setImageBitmap(bitmap);
+						} else {
+							// something went wrong
+						}
+					}
 				});
 
 			}
@@ -107,8 +110,19 @@ public class Edit_card extends Activity {
 				final EditText email= (EditText)findViewById(R.id.editcard_email);
 				final EditText telephone= (EditText)findViewById(R.id.editcard_telephone);
 				
+				ByteArrayOutputStream photoStream=new ByteArrayOutputStream();
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 100, photoStream);
+				byte[] photoArray=photoStream.toByteArray();
+
+				photoFile = new ParseFile("photo.jpeg", photoArray);
+				try {
+					photoFile.save();
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+
 				final ParseObject updatedCard = localCard; 
-			
+
 				updatedCard.saveInBackground(new SaveCallback() {
 					public void done(ParseException e) {
 						updatedCard.put("Name",name.getText().toString() );
@@ -119,15 +133,15 @@ public class Edit_card extends Activity {
 						updatedCard.put("Telephone",telephone.getText().toString() );
 						updatedCard.put("Photo", photoFile);
 						updatedCard.saveInBackground();
-						
+
 						Toast.makeText(getApplicationContext(),"Update Successful.",Toast.LENGTH_SHORT).show();
-						
+
 						/*Intent options=new Intent(getApplicationContext(), Options.class);
 						Bundle b = new Bundle();
 						b.putString("Username", localCard.getString("Username").toString());
 						options.putExtras(b);
 						startActivity(options);*/
-						
+
 						finish();
 					}
 				});
@@ -144,53 +158,77 @@ public class Edit_card extends Activity {
 		getMenuInflater().inflate(R.menu.edit_card, menu);
 		return true;
 	}
-	
+
 	public void onEditPhotoListener(View view) {
-		Intent intent = new Intent();
-		intent.setType("image/*");
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-    }
-	
-	public String getPath(Uri contentUri) {
-	    String res = null;
-	    String[] proj = { MediaStore.Images.Media.DATA };
-	    Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-	    if(cursor.moveToFirst()){;
-	       int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-	       res = cursor.getString(column_index);
-	    }
-	    cursor.close();
-	    return res;
+		startDialog();
 	}
-	
+
+	public String getPath(Uri contentUri) {
+		String res = null;
+		String[] proj = { MediaStore.Images.Media.DATA };
+		Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+		if(cursor.moveToFirst()){;
+		int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		res = cursor.getString(column_index);
+		}
+		cursor.close();
+		return res;
+	}
+
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
-			if (requestCode == SELECT_PICTURE) {
+			if (requestCode == GALLERY_PICTURE) {
 				Uri selectedImageUri = data.getData();
 
-				//MEDIA GALLERY
-				newImagePath = getPath(selectedImageUri);
-				imageView = (ImageView) findViewById(R.id.edit_photo_image);
-				bitmap = BitmapFactory.decodeFile(newImagePath);
-				imageView.setImageBitmap(bitmap);
-				ByteArrayOutputStream photoStream=new ByteArrayOutputStream();
-				bitmap.compress(Bitmap.CompressFormat.JPEG, 100, photoStream);
-				byte[] photoArray=photoStream.toByteArray();
-				photoFile = new ParseFile("photo.jpeg", photoArray);
-				try {
-					photoFile.save();
-				} catch (ParseException e1) {
-					e1.printStackTrace();
+				if (selectedImageUri != null) {
+					//MEDIA GALLERY
+					newImagePath = getPath(selectedImageUri);
+					imageView = (ImageView) findViewById(R.id.edit_photo_image);
+					bitmap = BitmapFactory.decodeFile(newImagePath);
+					imageView.setImageBitmap(bitmap);
+
+					//HERE IS WHAT WE WANT
+				//	Log.d("filepath", newImagePath);
+				//	Log.d("done", "kaam ho gaya... path bhi mil gaya aur photo bhi aa gaya! :D");
 				}
-				
-				
-				//HERE IS WHAT WE WANT
-				//Log.d("filepath", selectedImagePath);
-				//Log.d("done", "kaam ho gaya... path bhi mil gaya aur photo bhi aa gaya! :D");
-			}    
+				else {
+					Toast toast = Toast.makeText(this, "No Image is selected.", Toast.LENGTH_LONG);
+					toast.show();
+				}
+			}
+
+			else if (requestCode == CAMERA_PICTURE) {
+				if (data.getExtras() != null) {
+					// here is the image from camera
+					bitmap = (Bitmap) data.getExtras().get("data");
+					imageView.setImageBitmap(bitmap);
+				}
+			}
 		}
 	}
-	
+
+	private void startDialog() {
+		AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(this);
+		myAlertDialog.setTitle("Upload Pictures Option");
+		myAlertDialog.setMessage("How do you want to select your picture?");
+
+		myAlertDialog.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				pictureActionIntent = new Intent(Intent.ACTION_GET_CONTENT, null);
+				pictureActionIntent.setType("image/*");
+				pictureActionIntent.putExtra("return-data", true);
+				startActivityForResult(pictureActionIntent, GALLERY_PICTURE);
+			}
+		});
+
+		myAlertDialog.setNegativeButton("Camera", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				pictureActionIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(pictureActionIntent, CAMERA_PICTURE);
+			}
+		});
+		myAlertDialog.show();
+	}
+
 
 }
--- src/com/example/myvirtualcardwallet/MakeCard.java
+++ src/com/example/myvirtualcardwallet/MakeCard.java
@@ -3,6 +3,8 @@ package com.example.myvirtualcardwallet;
 import java.io.ByteArrayOutputStream;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -10,6 +12,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -23,11 +26,13 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 public class MakeCard extends Activity {
 	public Card myCard =new Card();
-	private static final int SELECT_PICTURE = 1;
 	private Bitmap bitmap;
 	private ImageView imageView;
 	private String selectedImagePath;
-	
+	private final int CAMERA_PICTURE = 1;
+	private final int GALLERY_PICTURE = 2;
+	private Intent pictureActionIntent = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,7 +55,7 @@ public class MakeCard extends Activity {
 		getMenuInflater().inflate(R.menu.make_card, menu);
 		return true;
 	}
-	
+
 	public void onSubmitListener(View view)
 	{
 		//EditText name = (EditText)findViewById(R.id.editName);
@@ -68,14 +73,14 @@ public class MakeCard extends Activity {
 		ByteArrayOutputStream photoStream=new ByteArrayOutputStream();
 		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, photoStream);
 		byte[] photoArray=photoStream.toByteArray();
-	
+
 		ParseFile photoFile = new ParseFile("photo.jpeg", photoArray);
 		try {
 			photoFile.save();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		ParseObject makecard = new ParseObject("Card");
 		makecard.put("Name", myCard.name);
 		makecard.put("Designation", myCard.designation);
@@ -85,59 +90,91 @@ public class MakeCard extends Activity {
 		makecard.put("Telephone", myCard.telephone);
 		makecard.put("Username", myCard.username);
 		makecard.put("Photo", photoFile);
-		//makecard.put("Mycard", myCard);
+		
 		makecard.saveInBackground();
-		//gameScore.put("playerName", "Sean Plott");
-		//gameScore.put("cheatMode", false);
-		//gameScore.saveInBackground();
-	
+		
+
 		Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
 		Intent home = new Intent(getApplicationContext(), Options.class);
-        home.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        Bundle b=new Bundle();
-        b=this.getIntent().getExtras();
-        home.putExtras(b);
-        startActivity(home);
+		home.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		Bundle b=new Bundle();
+		b=this.getIntent().getExtras();
+		home.putExtras(b);
+		startActivity(home);
 	}
-	
+
 	public void onAddPhotoListener(View view) {
-		Intent intent = new Intent();
-		intent.setType("image/*");
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-    }
-	
-	public String getPath(Uri contentUri) {
-	    String res = null;
-	    String[] proj = { MediaStore.Images.Media.DATA };
-	    Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-	    if(cursor.moveToFirst()){;
-	       int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-	       res = cursor.getString(column_index);
-	    }
-	    cursor.close();
-	    return res;
+		startDialog();
 	}
-	
+
+	public String getPath(Uri contentUri) {
+		String res = null;
+		String[] proj = { MediaStore.Images.Media.DATA };
+		Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+		if(cursor.moveToFirst()){;
+		int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		res = cursor.getString(column_index);
+		}
+		cursor.close();
+		return res;
+	}
+
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
-			if (requestCode == SELECT_PICTURE) {
+			if (requestCode == GALLERY_PICTURE) {
 				Uri selectedImageUri = data.getData();
 
-				//MEDIA GALLERY
-				selectedImagePath = getPath(selectedImageUri);
-				imageView = (ImageView) findViewById(R.id.add_photo_image);
-				bitmap = BitmapFactory.decodeFile(selectedImagePath);
-				imageView.setImageBitmap(bitmap);
-				
-				//HERE IS WHAT WE WANT
-				//Log.d("filepath", selectedImagePath);
-				//Log.d("done", "kaam ho gaya... path bhi mil gaya aur photo bhi aa gaya! :D");
-			}    
+				if (selectedImageUri != null) {
+					//MEDIA GALLERY
+					selectedImagePath = getPath(selectedImageUri);
+					imageView = (ImageView) findViewById(R.id.add_photo_image);
+					bitmap = BitmapFactory.decodeFile(selectedImagePath);
+					imageView.setImageBitmap(bitmap);
+
+					//HERE IS WHAT WE WANT
+					Log.d("filepath", selectedImagePath);
+					Log.d("done", "kaam ho gaya... path bhi mil gaya aur photo bhi aa gaya! :D");
+				}
+				else {
+					Toast toast = Toast.makeText(this, "No Image is selected.", Toast.LENGTH_LONG);
+					toast.show();
+				}
+			}
+
+			else if (requestCode == CAMERA_PICTURE) {
+				if (data.getExtras() != null) {
+					// here is the image from camera
+					bitmap = (Bitmap) data.getExtras().get("data");
+					imageView.setImageBitmap(bitmap);
+				}
+			}
 		}
 	}
-	
-	
-	
+
+	private void startDialog() {
+		AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(this);
+		myAlertDialog.setTitle("Upload Pictures Option");
+		myAlertDialog.setMessage("How do you want to select your picture?");
+
+		myAlertDialog.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				pictureActionIntent = new Intent(Intent.ACTION_GET_CONTENT, null);
+				pictureActionIntent.setType("image/*");
+				pictureActionIntent.putExtra("return-data", true);
+				startActivityForResult(pictureActionIntent, GALLERY_PICTURE);
+			}
+		});
+
+		myAlertDialog.setNegativeButton("Camera", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				pictureActionIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(pictureActionIntent, CAMERA_PICTURE);
+			}
+		});
+		myAlertDialog.show();
+	}
+
+
+
 
 }
