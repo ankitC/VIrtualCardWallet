6c8d837c2d02220f3c02ca23471db62f68fd55f5
Ankit <ankit.ac.chheda@gmail.com>
2013-04-29 20:32:05 -0400

    Edit Card and Add Card updated to render images

--- src/com/example/myvirtualcardwallet/Edit_card.java
+++ src/com/example/myvirtualcardwallet/Edit_card.java
@@ -1,19 +1,29 @@
 package com.example.myvirtualcardwallet;
 
+import java.io.ByteArrayOutputStream;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.parse.GetCallback;
+import com.parse.GetDataCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
@@ -21,6 +31,11 @@ import com.parse.SaveCallback;
 public class Edit_card extends Activity {
 	
 	public ParseObject localCard;
+	private static final int SELECT_PICTURE = 1;
+	private Bitmap bitmap;
+	private ImageView imageView;
+	private String newImagePath;
+	ParseFile photoFile;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -60,6 +75,20 @@ public class Edit_card extends Activity {
 
 				EditText telephone= (EditText)findViewById(R.id.editcard_telephone);
 				telephone.setText(foundCard.getString("Telephone").toString());
+				
+				
+				 photoFile= (ParseFile)foundCard.get("Photo");
+				 photoFile.getDataInBackground(new GetDataCallback() {
+				  public void done(byte[] data, ParseException e) {
+				    if (e == null) {
+				      bitmap=BitmapFactory.decodeByteArray(data, 0, data.length);
+				      ImageView image=(ImageView) findViewById(R.id.edit_photo_image);
+				      image.setImageBitmap(bitmap);
+				    } else {
+				      // something went wrong
+				    }
+				  }
+				});
 
 			}
 		});
@@ -88,7 +117,7 @@ public class Edit_card extends Activity {
 						updatedCard.put("Designation", designation.getText().toString());
 						updatedCard.put("Email",email.getText().toString() );
 						updatedCard.put("Telephone",telephone.getText().toString() );
-						
+						updatedCard.put("Photo", photoFile);
 						updatedCard.saveInBackground();
 						
 						Toast.makeText(getApplicationContext(),"Update Successful.",Toast.LENGTH_SHORT).show();
@@ -115,5 +144,53 @@ public class Edit_card extends Activity {
 		getMenuInflater().inflate(R.menu.edit_card, menu);
 		return true;
 	}
+	
+	public void onEditPhotoListener(View view) {
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+	
+	public String getPath(Uri contentUri) {
+	    String res = null;
+	    String[] proj = { MediaStore.Images.Media.DATA };
+	    Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+	    if(cursor.moveToFirst()){;
+	       int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	       res = cursor.getString(column_index);
+	    }
+	    cursor.close();
+	    return res;
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == SELECT_PICTURE) {
+				Uri selectedImageUri = data.getData();
+
+				//MEDIA GALLERY
+				newImagePath = getPath(selectedImageUri);
+				imageView = (ImageView) findViewById(R.id.edit_photo_image);
+				bitmap = BitmapFactory.decodeFile(newImagePath);
+				imageView.setImageBitmap(bitmap);
+				ByteArrayOutputStream photoStream=new ByteArrayOutputStream();
+				bitmap.compress(Bitmap.CompressFormat.JPEG, 100, photoStream);
+				byte[] photoArray=photoStream.toByteArray();
+				photoFile = new ParseFile("photo.jpeg", photoArray);
+				try {
+					photoFile.save();
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				
+				
+				//HERE IS WHAT WE WANT
+				//Log.d("filepath", selectedImagePath);
+				//Log.d("done", "kaam ho gaya... path bhi mil gaya aur photo bhi aa gaya! :D");
+			}    
+		}
+	}
+	
 
 }
